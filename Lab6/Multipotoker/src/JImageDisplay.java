5275f6ad99060ca5a5d23eeaaf96270617cb22c4
Nikita Sirotin <48451085+Zhihorka@users.noreply.github.com>
2021-05-25 02:38:50 +0300

    Add files via upload

---
+++ Lab6/Multipotoker/src/JImageDisplay.java
@@ -0,0 +1,38 @@
+//здесь все для изображения фрактала
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class JImageDisplay extends JComponent {
+    private BufferedImage image;
+
+    // объявление изображения и его параметров
+    public JImageDisplay(int width, int height) {
+        image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Dimension dimension = new Dimension(width, height);
+        super.setPreferredSize(dimension);
+    }
+
+    //отрисовка изображения
+    public void paintComponent(Graphics graphics) {
+        graphics.drawImage(image, 0, 0, image.getWidth(), image.getHeight(), null);
+    }
+
+    // удаление изобрадения фрактала
+    public void clearImage() {
+        for (int i = 0; i < image.getWidth(); i++) {
+            for (int j = 0; j < image.getHeight(); j++) {
+                drawPixel(i, j, 0);
+            }
+        }
+    }
+
+    // цвет для каждого пикселя
+    public void drawPixel(int x, int y, int rgbColor) {
+        image.setRGB(x, y, rgbColor);
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+}
